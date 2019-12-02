25d6eca1b30725a2b983d196b0fab882a850777c
Xavier Morales <x.m.1100010@gmail.com>
2019-12-02 18:40:05 +0100

    addPronvinceInSource

---
+++ Data/Migrations/20191202173830_addProvinceInSource.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wineapp.Data.Migrations
+{
+    public partial class addProvinceInSource : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Province",
+                table: "Sources",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Province",
+                table: "Sources");
+        }
+    }
+}
