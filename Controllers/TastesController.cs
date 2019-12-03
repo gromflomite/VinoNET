1b8c485662d6a2f3aabd0c384c6fa2f0fd1d0e99
Noelia MG <nminguezgonzalez@gmail.com>
2019-12-03 13:20:30 +0100

    Update tastes and filters

---
+++ Controllers/TastesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wineapp.Models.ViewModels;
+using Wineapp.Services;
+
+namespace Wineapp.Controllers
+{
+    public class TastesController : Controller
+    {
+        private readonly ITastes _tastesServices;
+
+        public TastesController(ITastes tastesServices)
+        {
+            _tastesServices = tastesServices;
+        }
+
+        // GET: Tastes
+        public ActionResult Survey(string userId)
+        {
+            TastesVM tvm = new TastesVM
+            {
+             
+
+            };
+
+            return View(tvm);
+        }
+
+        // GET: Tastes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Tastes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tastes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tastes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tastes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tastes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Tastes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/TastesVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wineapp.Models.ViewModels
+{
+    public class TastesVM
+    {
+        public ColourTaste ColourTaste { get; set; }
+        public SourceTaste SourceTaste { get; set; }
+        public SweetnessTaste SweetnessTaste { get; set; }
+        public List<Colour> ListColours { get; set; }
+        public List<Source> ListSources { get; set; }
+        public List<Sweetnes> ListSweetness { get; set; }
+        public List<ColourTaste> ColourTastes { get; set; }
+        public List<SourceTaste> SourceTastes { get; set; }
+        public List<SweetnessTaste> SweetnesTastes { get; set; }
+
+
+
+    }
+}
