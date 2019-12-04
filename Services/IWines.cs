d035481ecbd1a8094212692b66f541d799bd3bba
Noelia MG <nminguezgonzalez@gmail.com>
2019-12-04 12:30:17 +0100

    create IWine and WineServices

---
+++ Services/IWines.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wineapp.Models;
+
+namespace Wineapp.Services
+{
+    public interface IWines
+    {
+        public Task<List<Wine>> GetWinesAsync();
+        public Task<Wine> GetWineByIdAsync(int? id);
+        public Task CreateWineAsync(Wine wine);
+        public Task UpdateWineAsync(Wine wine);
+        public Task DeleteWineAsync(Wine wine);
+        public bool WineExists(int? id);
+    }
+}
