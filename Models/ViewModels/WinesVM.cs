8cf079460857e77ca72b859450292ff46622d46f
Xavier Morales <x.m.1100010@gmail.com>
2019-12-05 12:55:06 +0100

    combine Xavi 12:54 05/12/2019 noelia=>xavi

--- Models/ViewModels/WinesVM.cs
+++ Models/ViewModels/WinesVM.cs
@@ -9,13 +9,23 @@ namespace Wineapp.Models.ViewModels
     {
         public Wine Wine { get; set; }
         public Source Source { get; set; }
-        public List<Wine> ListWineTaste { get; set; }
-        public List<Wine> ListWineUserScore { get; set; }    
+        public List<Source> ListSources { get; set; }
 
-        public List< ColourTaste> ListUserColourTaste { get; set; }
+        //Lista de vinos aplicando los filtros adaptados a cada usuario
+        public List<Wine> ListWineTaste { get; set; }
+
+        //Lista de vinos segun su puntuación
+        public List<Wine> ListWineUserScore { get; set; }
+
+        //Lista de las tablas Tastes según el usuario
+        public List<ColourTaste> ListUserColourTaste { get; set; }
         public List<SourceTaste> ListUserSourceTaste { get; set; }
         public List<SweetnessTaste> ListUserSweetnessTaste { get; set; }
 
+        //Lista de vinos Tastes in View Details--> WinesController 
         public List<Wine> ListWinesTastesDetails { get; set; }
+
+        //Lista de vinos Tastes in View Sources--> WinesController 
+        public List<Wine> ListWinesTastesSources { get; set; }
     }
 }
