fbeafcfe10029d8776a4593f74b90d948704d786
maura rayo <rayomaura@gmail.com>
2019-12-09 17:10:12 +0100

    modify confirm email and register

--- Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -72,7 +72,7 @@ namespace Wineapp.Areas.Identity.Pages.Account
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Gracias por confirmar tu correo electrónico." : "Error al confirmar tu correo electrónico.";
+            StatusMessage = result.Succeeded ? "Gracias por registrarte en VinoNet." : "No te has podido registrar correctamente.";
             return Page();
         }
     }
